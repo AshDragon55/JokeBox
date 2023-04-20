1652ac9c5164d39b776f195ee28cae2440156351
AshDragon55 <ashert1000@gmail.com>
2023-04-20 16:55:50 -0400

    add to concole

--- imports/ShowJoke.js
+++ imports/ShowJoke.js
@@ -1,6 +1,6 @@
 import { Session } from 'meteor/session'
 
-Session.set('myJoke','test joke')
+// Session.set('myJoke','test joke')
 
 let getJoke = ()=>{
     // function to fetch/read data using a JavaScript promise
@@ -10,19 +10,26 @@ let getJoke = ()=>{
             Accept: 'application/json'
         }
     })
-
-.then(response => response.json())
-        .then(data => {
-            document.querySelector("#jokeId").innerHTML = data.id
-            document.querySelector("#jokeData").innerHTML = data.joke
-            console.log(data)
-        })
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#jokeId").value = data.id
+        document.querySelector("#jokeData").innerHTML = data.joke
+        console.log(data)
+    })
 }
 
 Template.Showjoke.onCreated(function () {
     getJoke()
-    setInterval(getJoke, 5000)
-})
+    setInterval(getJoke, 10000)
+}),
 
+
+Template.Showjoke.events({
+    'click .js-AddJoke'() {
+       let Jokeid = document.querySelector("#jokeId").value
+        let Joke = document.querySelector("#jokeData").innerHTML
+        console.log(Jokeid,Joke)
+    }
+})
 
 
